d086fb4e89d43f987ddf7f9d7749fba19c86e647
Tomi Lui <tomilui1221@gmail.com>
2021-04-01 17:46:19 -0700

    more info feature added, bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { TableComponent } from './table/table.component';
 import { NewReportButtonComponent } from './new-report-button/new-report-button.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NewReportButtonComponent } from './new-report-button/new-report-button.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
